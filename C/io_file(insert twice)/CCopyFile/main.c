238a16b1f3625be9f041ee32283ad2731cad3284
paco89lol <paco89lol@gmail.com>
2015-10-15 22:58:05 +0800

    a project added
    Add io_file(insert twice)

---
+++ C/io_file(insert twice)/CCopyFile/main.c
@@ -0,0 +1,32 @@
+#include <assert.h>
+#include <Windows.h>
+#include <stdio.h>
+
+#define BUFFER_SIZE 1024
+
+int main(int argc, char **argv)
+{
+	int i, copy_time;
+	char buffer[BUFFER_SIZE];
+	FILE *fp_read;
+	FILE *fp_write;
+
+	fopen_s(&fp_read, "text.txt", "r");
+	assert(fp_read);
+	fopen_s(&fp_write, "output_text.txt", "w");
+	assert(fp_write);
+	for (i = 0; i<copy_time; ++i)
+	{
+		fseek(fp_read,0,SEEK_SET);
+		fseek(fp_write,0,SEEK_END);
+		while (fgets(buffer, BUFFER_SIZE, fp_read) != NULL)
+		{
+			fputs(buffer, fp_write);
+		}
+		fputc('\n', fp_write);
+	}
+
+	fclose(fp_read);
+	fclose(fp_write);
+	return 0;
+}
---
+++ Cpp/io_file(insert twice)/CoutArgv/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <fstream>
+#include <assert.h>
+#include <Windows.h>
+
+#define BUFFER_SIZE 1024
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	int i, copy_time;
+	ifstream file_read("text.txt");
+	assert(file_read);
+	ofstream file_write("output_text.txt");
+	assert(file_write);
+
+	for (i = 0; i<copy_time; ++i)
+	{
+		file_read.seekg(0, ios::beg);
+		file_write.seekp(0, ios::end);
+		file_write << file_read.rdbuf() << endl;
+	}
+
+	Sleep(1000);
+	return 0;
+}
